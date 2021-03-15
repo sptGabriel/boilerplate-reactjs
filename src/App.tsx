0455da4520d19011b7372098bc00ee4649f29d6f
gabriel <gabrielcerqueira96@hotmail.com>
2021-03-15 12:16:27 -0300

    fix: deleted unnecessary files from public folder

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,13 @@ import React from "react";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
+    <div className="App h-full w-full bg-blue-500">
+      <header className="flex items-center bg-blue-200 flex-col">
+        <p className="flex items-center w-full justify-center bg-red-600 py-4 px-4 text-xl font-medium text-white">
           Welcome!!!!!!
         </p>
         <a
-          className="App-link"
+          className="flex items-center w-full justify-center bg-green-400 py-4 px-4 text-xl font-medium"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
